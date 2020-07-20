c39b3da4b29179ed5131f9cb6a5344659cc5a305
Kwang Ho Hur <hurxx018@gmail.com>
2020-07-20 09:21:21 -0500

    feat: Define Discriminator as a MLP

--- neural_networks.py
+++ neural_networks.py
@@ -3,3 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+class Discriminator(nn.Module):
+    
+    def __init__(
+        self,
+        input_size,
+        hidden_dim,
+        output_size
+        ):
+        super(Discriminator, self).__init__()
+        self.input_size = input_size
+        self.hidden_dim = hidden_dim
+        self.output_size = output_size
+
+        # define all layers
+        self.fc1 = nn.Linear(input_size, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_size)
+
+        self.leakyrelu = nn.LeakyReLU(0.1)
+
+    def forward(
+        self, 
+        x
+        ):
+        # flatten imaga
+        x = x.view(-1, self.input_size)
+        # pass x through all layers
+        x = self.fc1(x)
+        x = self.leakyrelu(x)
+        x = self.fc2(x)
+
+        return x
