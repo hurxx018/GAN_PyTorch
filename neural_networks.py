b556e07166e14ada3a3735d0311c832f82f224c3
Kwang Ho Hur <hurxx018@gmail.com>
2020-07-20 19:16:21 -0500

    feat: Define a Generator class

--- neural_networks.py
+++ neural_networks.py
@@ -34,3 +34,38 @@ class Discriminator(nn.Module):
         x = self.fc2(x)
 
         return x
+
+
+class Generator(nn.Module):
+
+    def __init__(
+        self,
+        input_size,
+        hidden_dim,
+        output_size
+        ):
+        super(Generator, self).__init__()
+        self.input_size = input_size
+        self.hidden_dim = hidden_dim
+        self.output_size = output_size
+
+        # define all layers
+        self.fc1 = nn.Linear(input_size, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_size)
+
+        self.leakyrelu = nn.LeakyReLU(0.1)
+        self.tanh = nn.Tanh()
+
+    def forward(
+        self, 
+        x
+        ):
+        # pass x through all layers
+        x = self.fc1(x)
+        x = self.leakyrelu(x)
+        x = self.fc2(x)
+        # final layer should have tanh applied
+        x = self.tanh(x)
+
+        return x
+
