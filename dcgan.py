b7bc798cdf9e42c626deec815062c972c97de368
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-01 21:51:18 -0500

    feat: Update Discriminator
    Define three conv2d layers where channels are 32, 64, 128.

--- dcgan.py
+++ dcgan.py
@@ -6,8 +6,13 @@ import torch.optim as optim
 class Discriminator(nn.Module):
 
     def __init__(self):
-        super().__init__()
-
+        super().__init__(
+            hidden_dim = 32
+        )
+        self.hidden_dim = hidden_dim
+        self.conv1 = nn.Conv2d(3, self.hidden_dim, 2, 2)
+        self.conv2 = nn.Conv2d(self.hidden_dim, self.hidden_dim*2, 2, 2)
+        self.conv3 = nn.Conv2d(self.hidden_dim*2, self.hidden_dim*4, 2, 2)
 
     def forward(self, x):
 
