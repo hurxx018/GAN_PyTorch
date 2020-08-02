705c4e4b3822dc7d0c17d6e4a24494efc29f9ef6
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-01 21:37:22 -0500

    feat: Define Discriminator and Generator classes
    Begin the implementation of Discriminator and Generator.

---
+++ dcgan.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, x):
+
+
+        return
+
+
+class Generator(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+
+
+        return
