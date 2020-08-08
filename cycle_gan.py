7989682e46f2ed8109e31a6452e519c1f5529e7d
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-07 22:36:44 -0500

    feat: Define get_data_loader
    Create two DataLoaders; training and testing.

--- cycle_gan.py
+++ cycle_gan.py
@@ -1,9 +1,17 @@
 """ #CycleGAN, Image-to-Image Translation
 
 """
+import os
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torch.utils.data import DataLoader
+
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
 def conv(
     in_channels,
@@ -33,11 +41,17 @@ class Discriminator(nn.Module):
         ):
         super(Discriminator, self).__init__()
 
+        self.input_in_channels = input_in_channels
+        self.conv_dim = conv_dim
+
+        # Define all convolutional layers
         self.conv1 = conv(self.input_in_channels, self.conv_dim*1, 4, 2, 2, batch_norm=False)
         self.conv2 = conv(self.conv_dim*1, self.conv_dim*2, 4, 2, 2, batch_norm = True)
         self.conv3 = conv(self.conv_dim*2, self.conv_dim*4, 4, 2, 2, batch_norm = True)
         self.conv4 = conv(self.conv_dim*4, self.conv_dim*8, 4, 2, 2, batch_norm = True)
-        self.conv5 = conv(self.conv_dim*8, 1, batch_norm = False)
+
+        # kernel_size = 128/2/2/2/2 = 8
+        self.conv5 = conv(self.conv_dim*8, 1, 8, 0, 0, batch_norm = False)
 
         nn.relu = nn.ReLU()
 
@@ -46,5 +60,45 @@ class Discriminator(nn.Module):
         x
         ):
 
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.relu(self.conv4(x))
+        out = self.conv5(x)
 
         return out
+
+
+
+
+
+
+
+
+
+def get_data_loader(
+    image_type,
+    image_dir,
+    image_size = 128,
+    batch_size = 16,
+    num_workers = 0
+    ):
+
+    # resize and normalize the images
+    transform = transforms.Compose([transforms.Resize(image_size), # resize to 128x128
+                                    transforms.ToTensor()])
+
+    # get training and test directories
+    image_path = os.path.join('.', image_dir)
+    train_path = os.path.join(image_path, image_type)
+    test_path = os.path.join(image_path, 'test_{}'.format(image_type))
+
+    # define datasets using ImageFolder
+    train_dataset = datasets.ImageFolder(train_path, transform)
+    test_dataset = datasets.ImageFolder(test_path, transform)
+
+    # create and return DataLoaders
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, test_loader    
